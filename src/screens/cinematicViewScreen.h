a53f5d724b24b355fd3fc4e226b2b82e7daf9f39
Mathieu Giguere <mathieu.giguere@vantrix.com>
2019-08-04 16:27:32 -0400

    Missing default initiation of vars

--- src/screens/cinematicViewScreen.h
+++ src/screens/cinematicViewScreen.h
@@ -19,24 +19,24 @@ private:
     GuiSelector* camera_lock_selector;
     GuiToggleButton* camera_lock_toggle;
     GuiToggleButton* camera_lock_tot_toggle;
-    float min_camera_distance;
-    float max_camera_distance;
+    float min_camera_distance = 10.0f;
+    float max_camera_distance = 1000.f;
     sf::Vector2f camera_rotation_vector;
     sf::Vector2f camera_destination;
-    float angle_yaw;
-    float angle_pitch;
+    float angle_yaw = 0.0f;
+    float angle_pitch = 0.0f;
 
     sf::Vector2f diff_2D;
     sf::Vector3f diff_3D;
-    float distance_2D;
-    float distance_3D;
+    float distance_2D = 10.0f;
+    float distance_3D = 10.0f;
 
     sf::Vector2f target_position_2D;
     sf::Vector3f target_position_3D;
     // camera_position is a Vector3, so no need to declare one here.
     sf::Vector2f camera_position_2D;
-    float target_rotation;
-    float target_velocity;
+    float target_rotation = 0.0f;
+    float target_velocity = 0.0f;
 
     P<SpaceObject> target_of_target;
 
@@ -44,9 +44,9 @@ private:
     sf::Vector3f tot_position_3D;
     sf::Vector2f tot_diff_2D;
     sf::Vector3f tot_diff_3D;
-    float tot_angle;
-    float tot_distance_2D;
-    float tot_distance_3D;
+    float tot_angle = 0.0f;
+    float tot_distance_2D = 0.0f;
+    float tot_distance_3D = 0.0f;
 
 public:
     CinematicViewScreen();
